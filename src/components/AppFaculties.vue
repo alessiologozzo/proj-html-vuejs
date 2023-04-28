7495549396298e5fe07d30251e9efbb917d7061c
alessiologozzo <alessio.logozzo@libero.it>
2023-04-28 13:55:54 +0200

    fixed image deformation in AppFaculties

--- src/components/AppFaculties.vue
+++ src/components/AppFaculties.vue
@@ -15,7 +15,7 @@
         </div>
 
         <div class="container py-4">
-            <div class="row justify-content-center">
+            <div class="row justify-content-center align-items-center">
                 <div class="col-12 col-md-6 d-flex justify-content-center pt-3">
                     <img :src="getUrl()" alt="err">
                 </div>
