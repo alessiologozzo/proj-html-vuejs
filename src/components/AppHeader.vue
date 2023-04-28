f969b57ccc1652494a082af65a8d21e12e5c2af7
alessiologozzo <alessio.logozzo@libero.it>
2023-04-28 12:50:59 +0200

    added background slider in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -38,11 +38,15 @@
         </div>
 
         <aside class="d-none d-md-flex">
-            <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
-            <a href="#"><i class="fa-regular fa-newspaper"></i></a>
-            <a href="#"><i class="fa-solid fa-compass"></i></a>
+            <i @click="showBg1()" class="fa-solid fa-cart-shopping"></i>
+            <i @click="showBg2()" class="fa-regular fa-newspaper"></i>
+            <i @click="showBg3()" class="fa-solid fa-compass"></i>
         </aside>
         <img src="../assets/img/Wave-1.png" alt="wave" class="al-wave">
+
+        <div id="bg-container-1" class="al-bg-1"></div>
+        <div id="bg-container-2" class="al-bg-2 al-opacity"></div>
+        <div id="bg-container-3" class="al-bg-3 al-opacity"></div>
     </header>
 </template>
 
@@ -55,12 +59,95 @@
         
         data() {
             return {
-                links: navLinks
+                links: navLinks,
+                activeBg: 1,
+                interval: null
             }
         },
 
         methods: {
-            showMenu: showMenu
+            showMenu: showMenu,
+
+            showBg1(){
+                let container1 = document.getElementById("bg-container-1");
+                let container2 = document.getElementById("bg-container-2");
+                let container3 = document.getElementById("bg-container-3");
+                
+                if(container1.classList.contains("al-opacity"))
+                    container1.classList.remove("al-opacity");
+
+                if(!container2.classList.contains("al-opacity"))
+                    container2.classList.add("al-opacity");
+
+                if(!container3.classList.contains("al-opacity"))
+                    container3.classList.add("al-opacity");
+
+                this.activeBg = 1;
+                clearTimeout(this.interval);
+                this.interval = setTimeout(this.changeBg, 7000);
+            },
+
+            showBg2(){
+                let container1 = document.getElementById("bg-container-1");
+                let container2 = document.getElementById("bg-container-2");
+                let container3 = document.getElementById("bg-container-3");
+                
+                if(!container1.classList.contains("al-opacity"))
+                    container1.classList.add("al-opacity");
+
+                if(container2.classList.contains("al-opacity"))
+                    container2.classList.remove("al-opacity");
+
+                if(!container3.classList.contains("al-opacity"))
+                    container3.classList.add("al-opacity");
+
+                this.activeBg = 2;
+                clearTimeout(this.interval);
+                this.interval = setTimeout(this.changeBg, 7000);
+            },
+
+            showBg3(){
+                let container1 = document.getElementById("bg-container-1");
+                let container2 = document.getElementById("bg-container-2");
+                let container3 = document.getElementById("bg-container-3");
+                
+                if(!container1.classList.contains("al-opacity"))
+                    container1.classList.add("al-opacity");
+
+                if(!container2.classList.contains("al-opacity"))
+                    container2.classList.add("al-opacity");
+
+                if(container3.classList.contains("al-opacity"))
+                    container3.classList.remove("al-opacity");
+
+                this.activeBg = 3;
+                clearTimeout(this.interval);
+                this.interval = setTimeout(this.changeBg, 7000);
+
+            },
+
+            changeBg(){
+                switch(this.activeBg){
+                    case 1:
+                        this.showBg2();
+                        this.activeBg = 2;
+                        break;
+
+                    case 2:
+                        this.showBg3();
+                        this.activeBg = 3;
+                        break;
+
+                    case 3:
+                        this.showBg1();
+                        this.activeBg = 1;
+                        break;
+                }
+            }
+        },
+
+        mounted() {
+            this.interval = setTimeout(this.changeBg, 7000);
         },
     }
 </script>
@@ -69,12 +156,40 @@
     @import "../assets/utilities.scss";
     
     header{
-        background: linear-gradient(rgb(229, 103, 104, 0.8), rgb(229, 103, 104, 0.8)), url("../assets/img/theme_slider2_bg-1.jpg");
+        background: linear-gradient(rgb(229, 103, 104, 0.8), rgb(229, 103, 104, 0.8));
         background-size: cover;
         color: white;
         overflow-x: hidden;
         padding-top: 1rem;
         position: relative;
+    }
+
+    .al-bg-1, .al-bg-2, .al-bg-3{
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-size: cover;
+        z-index: -10;
+        transition: opacity 2s;
+    }
+
+    .al-bg-1{
+        background-image: url("../assets/img/theme_slider2_bg-1.jpg");
+    }
+
+    .al-bg-2{
+        background-image: url("../assets/img/theme_slider3_bg-1.jpg");
+    }
+
+    .al-bg-3{
+        background-image: url("../assets/img/theme_slider1_bg-1.jpg");
+    }
+
+    .al-opacity{
+        opacity: 0;
+        transition: opacity 2s;
     }
 
     .al-logo{
@@ -128,20 +243,19 @@
         transform: translateY(-50%);
         display: flex;
         flex-flow: column nowrap;
-        gap: 0.8rem;
+        gap: 1.2rem;
         font-size: 1.4rem;
         background-color: $app_gold;
         padding: 0.9rem 0.9rem;
         border-radius: 0 5px 5px 0;
         box-shadow: 2px 2px 30px 2px #a55053;
 
-        a{
-            color: white;
-            text-decoration: none;
+        i{
             transition: transform 300ms;
+            cursor: pointer;
 
             &:hover{
-                transform: scale(1.2);
+                transform: scale(1.15);
             }
         }
     }
